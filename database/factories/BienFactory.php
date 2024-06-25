ef1cd7873b37851f29cbd309b3f4283248efd9a0
veronique derick <derickveronique@gmail.com>
2024-06-25 14:52:37 -0400

    create fake data

--- database/factories/BienFactory.php
+++ database/factories/BienFactory.php
@@ -17,6 +17,14 @@ class BienFactory extends Factory
     public function definition(): array
     {
         return [
+            'user_id'=>fake()->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'category_id'=>fake()->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'name'=>fake()->word(),
+            'description'=>fake()->sentence(),
+            'price'=>fake()->randomFloat(),
+            'city'=>fake()->streetAddress(),
+            'image'=>fake()->imageUrl(640 , 480 , 'house', true),
+
             //
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bien;
 use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Category::factory(10)->create();
+        Bien::factory(100)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
